16e339a2ddd2ea16f90685bf44f3819132de7f0d
atul singh <atul.singh@shephertz.co.in>
2013-09-30 19:27:36 +0530

    Adding file

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Hello World</title>
+ </head>
+ <body>
+ <?php 
+  echo '<p>Hello World, Welcome to App42Paas!!THANKS :) October1</p>';
+  $file=fopen("welcome.txt","w");
+ ?> 
+ </body>
+</html>
